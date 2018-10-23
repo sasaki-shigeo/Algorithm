74b4900b345a0bd62f3c18fc3a84db9aed69af2f
SASAKI Shigeo <sasaki-shigeo@acm.org>
2018-10-23 21:21:21 +0900

    HashTable added

---
+++ HashTable.java
@@ -0,0 +1,266 @@
+import java.util.*;
+
+public class HashTable<K, V> extends AbstractMap<K, V> {
+    protected SimpleEntry<K, V>[] table_;
+    protected int count_ = 0;
+    private HashTable<K, V> self = this;
+
+    public HashTable(int n) {
+        table_ = new SimpleEntry[n];
+    }
+
+    public HashTable() {
+        this(10);
+    }
+
+    protected int hash(K key) {
+        return key.hashCode() & 0x3FFFFFFF;
+    }
+    protected int hash2(K key) {
+        return (key.hashCode() >>> 16) + 7;
+    }
+
+    @Override
+    public int size() {
+        return count_;
+    }
+
+    public int capacity() {
+        return table_.length;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < capacity(); i++) {
+            table_[i] = null;
+        }
+        count_ = 0;
+    }
+
+    final int MAX_RETRY = 10;
+
+    int searchGET(K key) {
+        return searchGET(key, table_);
+    }
+
+    int searchGET(K key, Map.Entry<K, V>[] table) {
+        int ix = hash(key) % capacity();
+        for (int i = 0; i < MAX_RETRY; i++) {
+            if (table[ix] == null)
+                return -1;
+            else {
+                K k = table[ix].getKey();
+                if (k != null && k.equals(key)) 
+                    return ix;
+               
+            }
+            ix = (ix + hash2(key)) % capacity();
+        }
+        throw new RuntimeException("Hashkey loop");
+    }
+
+    int searchPUT(K key) {
+        return searchPUT(key, table_);
+    }
+
+    int searchPUT(K key, Map.Entry<K, V>[] table) {
+        int ix = hash(key) % capacity();
+
+        for (int i = 0; i < MAX_RETRY; i++) {
+            if (table[ix] == null)
+                return ix;
+            else {
+                K k = table[ix].getKey();
+                if (k == null || k.equals(key))
+                    return ix;
+            }
+            ix = (ix + hash2(key)) % capacity();
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return searchGET((K)key) >= 0;
+    }
+
+    @Override
+    public V get(Object key) {
+        int ix = searchGET((K)key);
+        if (ix >= 0)
+            return table_[ix].getValue();
+        else
+            return null;
+    }
+
+    @Override
+    public V getOrDefault(Object key, V defaultValue) {
+        int ix = searchGET((K)key);
+        if (ix >= 0)
+            return table_[ix].getValue();
+        else
+            return defaultValue;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        if (size() > capacity() / 2)
+            rehash();
+
+        int ix = searchPUT(key);
+        if (ix >= 0) {
+            V result = get(key);
+
+            if (table_[ix] == null)
+                table_[ix] = new SimpleEntry(key, value);
+            else
+                table_[ix].setValue(value);
+
+            count_++;
+            return result;
+        }
+        else {
+            System.err.println("Emergency rehash:" + key + ", " + value);
+            rehash();
+            put(key, value);
+            return null;
+        }
+    }
+
+    class EntryIterator implements Iterator<Map.Entry<K, V>> {
+        private int ix = 0;
+        
+        EntryIterator() {
+            findNext();
+        }
+
+        private void findNext() {
+            while (ix < table_.length &&
+                   (table_[ix] == null ||
+                    table_[ix].getKey() == null)) {
+                ix++;
+            }
+        }
+
+        public boolean hasNext() {
+            return ix < table_.length;
+        }
+
+        public Map.Entry<K, V> next() {
+            if (! hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            Map.Entry<K, V> result = table_[ix++];
+            findNext();
+            return result;
+        }
+    }
+
+    public Set<Map.Entry<K, V>> entrySet() {
+        return new AbstractSet<Map.Entry<K, V>>() {
+            public int size() {
+                return count_;
+            }
+            @Override
+            public Iterator<Map.Entry<K, V>> iterator() {
+                return new EntryIterator();
+            }
+        };
+    }
+
+    class KeyIterator implements Iterator<K> {
+        EntryIterator it;
+
+        KeyIterator() {
+            it = new EntryIterator();
+        }
+
+        public boolean hasNext() {
+            return it.hasNext();
+        }
+
+        public K next() {
+            return it.next().getKey();
+        }
+    }
+
+    public Set<K> keySet() {
+        return new AbstractSet<K>() {
+            public int size() {
+                return count_;
+            }
+            @Override
+            public Iterator<K> iterator() {
+                return new KeyIterator();
+            }
+
+            public boolean contains(Object key) {
+                return self.containsKey((K) key);
+            }
+        };
+    }
+
+    void rehash() {
+        System.err.println("rehash is called.");
+        SimpleEntry<K, V> [] newTable;
+
+        retry: do {
+            newTable = new SimpleEntry[2 * capacity() + 1];
+            for (Map.Entry<K, V> entry :entrySet()) {
+                int ix = searchPUT(entry.getKey(), newTable);
+                if (ix < 0)
+                    continue retry;
+
+                newTable[ix] = (SimpleEntry)entry;
+            }
+        } while (false);
+        table_ = newTable;
+    }
+
+    public static void main(String[] args) {
+        HashTable<String, String> table = new HashTable<>();
+        table.put("Japan", "Tokyo");
+        table.put("US", "Washington");
+        table.put("UK", "London");
+        table.put("France", "Paris");
+        table.put("Italy", "Rome");
+        table.put("Germany", "Berlin");
+        table.put("Russia", "Moscow");
+        table.put("Holand", "Amsterdom");
+        System.out.println(table);
+        table.put("Spain", "Madrid");
+        table.put("Denmark", "Copenhagen");
+        table.put("Greece", "Athen");
+        System.out.println(table);
+
+        for (String country: table.keySet()) {
+            System.out.printf("%s: %8x %d %d\n", country, country.hashCode(), table.hash(country) % 10, table.hash2(country) % 10);
+            System.out.printf("%s: %8x %d %d\n", country, country.hashCode(), table.hash(country) % 21, table.hash2(country) % 21);
+        }
+    }
+}
+
+class SingleLinkedList<T> {
+    private T car;
+    private SingleLinkedList<T> cdr;
+
+    SingleLinkedList(T car, SingleLinkedList<T> cdr) {
+        this.car = car;
+        this.cdr = cdr;
+    }
+
+    T head() {
+        return car;
+    }
+
+    SingleLinkedList<T> tail() {
+        return cdr;
+    }
+}
+
+class AList<K, V> extends SingleLinkedList<Map.Entry<K, V>> {
+    AList(K key, V value, SingleLinkedList<Map.Entry<K, V>> cdr) {
+        super(new AbstractMap.SimpleEntry(key, value), cdr);
+    }
+}
---
+++ HashTable.scala
@@ -0,0 +1,109 @@
+import scala.collection.mutable
+
+class HashTable[K, V](n: Int) extends mutable.AbstractMap[K, V] {
+    protected var table = mutable.ArraySeq.fill(n)(Nil:List[(K, V)])
+
+    protected var count = 0
+
+    def this() = this(10)
+
+    override def size: Int = count
+    def capacity: Int = table.size
+
+    override def clear {
+        for (i <- 0 until capacity) {
+            table(i) = Nil
+        }
+        count = 0
+    }
+
+    def iterator = new Iterator[(K, V)] {
+        private var ix = 0
+
+        while (ix < table.size && table(ix).isEmpty) {
+            ix += 1
+        }
+
+        private var p: List[(K, V)] =
+            if (ix < table.size) table(ix) else Nil
+
+        def hasNext: Boolean = ! p.isEmpty
+
+        private def findNextBin {
+            ix += 1
+            if (ix >= table.size)
+                p = Nil
+            else if (table(ix).isEmpty)
+                findNextBin
+            else
+                p = table(ix)
+        }
+
+        def next(): (K, V) = {
+            val result = p.head
+            p = p.tail
+            if (p.isEmpty) {
+                findNextBin
+            }
+            result
+        }
+    }
+
+    private def hash(key: K): Int =
+        (key.hashCode & 0x7FFFFFFF) % table.size
+
+    def get(key: K): Option[V] = {
+        for {
+            (k, v) <- table(hash(key)).find(_._1 == key)
+        } yield v
+    }
+
+    def +=(kv: (K, V)) = {
+        def replaceOrAdd(alist: List[(K, V)]): List[(K, V)] = alist match {
+            case Nil => { count += 1; List(kv) }
+            case (k, v)::xs if k == kv._1 => kv::xs
+            case x::xs => x::replaceOrAdd(xs)
+        }
+
+        val ix = hash(kv._1)
+        table(ix) = replaceOrAdd(table(ix))
+        this
+    }
+
+
+    def -=(key: K) = {
+        def removeIfExists(alist: List[(K, V)]): List[(K, V)] = alist match {
+            case Nil => Nil
+            case (k, v)::xs if k == key => { count -= 1; xs }
+            case x::xs => x::removeIfExists(xs)
+        }
+
+        val ix = hash(key)
+        table(ix) = removeIfExists(table(ix))
+        this
+    }
+
+    private def rehash {
+        val newTable = mutable.ArraySeq.fill(2 * capacity)(Nil:List[(K, V)])
+        for (kv <- this) {
+            val ix = hash(kv._1)
+            newTable(ix) = kv :: newTable(ix)
+        }
+        table = newTable
+    }
+}
+
+object HashTable {
+    def main(args: Array[String]) {
+        val table = new HashTable[String, String]
+        println(table)
+        table += "Japan" -> "Tokyo"
+        println(table)
+        table ++= Seq("US"->"Washington", "France"->"Paris", "UK"->"London",  "Itary"->"Rome")
+        println(table)
+        table ++= Seq("Germany"->"Berlin", "Holand"->"Amsterdom")
+        println(table)
+        table.rehash
+        println(table)
+    }
+}
