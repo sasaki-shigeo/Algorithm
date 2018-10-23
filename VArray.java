a4ddaff64418418f569e29d970d1255bec73e84b
SASAKI Shigeo <sasaki-shigeo@acm.org>
2018-10-23 21:23:51 +0900

    bugfix of Constructor

--- VArray.java
+++ VArray.java
@@ -5,41 +5,50 @@ public class VArray<T> extends AbstractList<T> {
     protected int limit = 0;
 
     public VArray(int n) {
-	array_ = (T [])new Object[n];
+        array_ = (T [])new Object[n];
     }
 
     public VArray() {
-	new VArray(10);
+        this(10);
     }
 
     public int capacity() {
-	return array_.length;
+        return array_.length;
     }
 
     public int size() {
-	return limit;
+        return limit;
     }
 
     public T get(int n) {
-	return array_[n];
+        return array_[n];
     }
 
     public T set(int n, T x) {
-	T before = array_[n];
-	array_[n] = x;
-	return before;
+        T before = array_[n];
+        array_[n] = x;
+        return before;
     }
 
     public boolean add(T x) {
-	if (limit >= capacity()) {
-	    T [] newArray = (T [])new Object[2 * limit];
-	    for (int i = 0; i < limit; i++) {
-		newArray[i] = array_[i];
-	    }
-	    array_ = newArray;
-	}
+        if (limit >= capacity()) {
+            T [] newArray = (T [])new Object[2 * limit];
+            for (int i = 0; i < limit; i++) {
+                newArray[i] = array_[i];
+            }
+            array_ = newArray;
+        }
 
-	array_[limit++] = x;
-	return true;
+        array_[limit++] = x;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        VArray xs = new VArray<Integer>();
+        xs.add(1);
+        xs.add(2);
+        xs.add(4);
+        xs.add(8);
+        System.out.println(xs);
     }
 }
