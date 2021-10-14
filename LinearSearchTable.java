79191c0650aa100f448ef8914ab7f5f7947113f1
SASAKI Shigeo <sasaki-shigeo@acm.org>
2021-10-14 23:28:43 +0900

    LinearSearchTable is now a subclass of AbstractMap

--- LinearSearchTable.java
+++ LinearSearchTable.java
@@ -1,8 +1,12 @@
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.AbstractMap;
 import java.util.AbstractMap.SimpleEntry;
+import java.util.AbstractSet;
 
-public class LinearSearchTable<K, V> {
+public class LinearSearchTable<K, V> extends AbstractMap<K, V> {
 
     class Entry extends SimpleEntry<K, V> {
         K key;
@@ -15,7 +19,7 @@ public class LinearSearchTable<K, V> {
         }
     }
 
-    protected ArrayList<Entry> table_;
+    protected ArrayList<Map.Entry<K,V>> table_;
 
     public LinearSearchTable() {
         table_ = new ArrayList<>();
@@ -25,43 +29,54 @@ public class LinearSearchTable<K, V> {
         table_ = new ArrayList<>(n);
     }
 
-    public int size() {
-        return table_.size();
-    }
+    public Set<Map.Entry<K, V>> entrySet() {
+	return new AbstractSet<Map.Entry<K, V>>() {
+	    public int size() {
+		return table_.size();
+	    }
 
+	    public Iterator<Map.Entry<K, V>> iterator() {
+		return table_.iterator();
+	    }
+	};
+    }    
+
+    @Override
     public void clear() {
         table_.clear();
     }
 
-    protected int lin_search(K key) {
+    protected int lin_search(Object key) {
         if (key == null)
             return -1;
 
         for (int ix = 0; ix < table_.size(); ix++) {
-            if (key.equals(table_.get(ix).key))
+            if (key.equals(table_.get(ix).getKey()))
                 return ix;
         }
         return -1;
     }
 
-    protected int index(K key) {
+    protected int index(Object key) {
         return lin_search(key);
     }
 
-    public V get(K key) {
+    @Override
+    public V get(Object key) {
         int ix = index(key);
         if (ix >= 0)
-            return table_.get(ix).value;
+            return table_.get(ix).getValue();
         else
             return null;
     }
 
+    @Override
     public V put(K key, V value) {
         int ix = index(key);
         if (ix >= 0) {
-            Entry ent = table_.get(ix);
-            V before = ent.value;
-            ent.value = value;
+            Map.Entry<K, V> ent = table_.get(ix);
+            V before = ent.getValue();
+            ent.setValue(value);
             return before;
         }
         else {
