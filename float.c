436cb9923df9df4b5b07648455e2d3155a50af1b
SASAKI Shigeo <sasaki-shigeo@acm.org>
2023-06-11 02:44:40 +0900

    output HEX Float value.

--- float.c
+++ float.c
@@ -3,26 +3,31 @@
 #include <float.h>
 
 int main() {
-    puts("float\n");
+    puts("float:");
     printf("significand bits = %d\n", FLT_MANT_DIG);
     printf("min exp part = %d\n", FLT_MIN_EXP);
     printf("max exp part = %d\n", FLT_MAX_EXP);
-    printf("max = %g (%a)\n", FLT_MAX, FLT_MAX);
-    printf("min positive normal = %g (%a)\n", FLT_MIN, FLT_MIN);
-    printf("machine epsilon = %g (%a) = 0x0.000002P0\n", FLT_EPSILON, FLT_EPSILON);
-    printf("min positive denormal = %g\n", (float)FLT_MIN * (float)FLT_EPSILON);
-    puts("\ndouble\n");
+    printf("machine epsilon = %G (%a) = 0x0.000002P0\n", FLT_EPSILON, FLT_EPSILON);
+    printf("max = %G (%a)\n", FLT_MAX, FLT_MAX);
+    printf("min positive normal = %G (%a)\n", FLT_MIN, FLT_MIN);
+    float flt_min_denormal = (float)FLT_EPSILON * (float)FLT_MIN;
+    printf("min positive denormal = %G (%a)\n", flt_min_denormal, flt_min_denormal);
+    puts("\ndouble:");
     printf("significand bits = %d\n", DBL_MANT_DIG);
     printf("min exp part = %d\n", DBL_MIN_EXP);
     printf("max exp part = %d\n", DBL_MAX_EXP);
-    printf("min positive normal = %g\n", DBL_MIN);
-    printf("machine epsilon = %g\n", DBL_EPSILON);
-    puts("\nlong double\n");
+    printf("machine epsilon = %G (%a)\n", DBL_EPSILON, DBL_EPSILON);
+    printf("min positive normal = %G (%a)\n", DBL_MIN, DBL_MIN);
+    double dbl_min_denormal = DBL_EPSILON * DBL_MIN;
+    printf("min positive denormal = %G (%a)\n", dbl_min_denormal, dbl_min_denormal);
+    puts("\nlong double:");
     printf("significand bits = %d\n", LDBL_MANT_DIG);
     printf("min exp part = %d\n", LDBL_MIN_EXP);
     printf("max exp part = %d\n", LDBL_MAX_EXP);
-    printf("min positive normal = %Lg\n", LDBL_MIN);
-    printf("machine epsilon = %Lg\n", LDBL_EPSILON);
+    printf("machine epsilon = %LG (%La)\n", LDBL_EPSILON, LDBL_EPSILON);
+    printf("min positive normal = %LG (%La)\n", LDBL_MIN, LDBL_MIN);
+    long double ldbl_min_denormal = LDBL_EPSILON * LDBL_MIN;
+    printf("min positive denormal = %LG (%La)\n", ldbl_min_denormal, ldbl_min_denormal);
 
     exit(0);
 }
