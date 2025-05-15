d4ce7da3018ec02a5cf8c6adfdf93cea4730260f
SASAKI Shigeo <sasaki-shigeo@acm.org>
2025-05-16 04:19:02 +0900

    table driven version (under construction)

--- String2int/ParseInt.scala
+++ String2int/ParseInt.scala
@@ -1,23 +1,75 @@
 import java.io.Reader
 import java.io.StringReader
 
-sealed class State {
-    val EOF = -1
-    def transit(c: Char): State
-}
+enum State { case START, ZERO, DEC, OCT, BIN, X, HEX }
 
-case object START extends State {
-    def transit(c: Char): State = {
-        if (c == '0') {
-            ZERO
-        }
-        else if ('1' <= c && c <= '9') {
-            DEC
-        }
+
+class StringIterator(cs: CharSequence) extends Iterator[Char] {
+    var ix = 0
+    def hasNext: Boolean = ix < cs.length
+    def next(): Char = {
+        val c = cs.charAt(ix)
+        ix += 1
+        c
     }
 }
-case class ZERO  extends State
-case class DEC   extends State
-case class OCT   extends State
-case class X     extends State
-case class HEX   extends State
+
+class ReaderIterator(reader: Reader) extends Iterator[Char] {
+    val EOF = -1               // the mark of End of File   
+    var c = reader.read()
+    def hasNext: Boolean = (c != EOF)
+    def next(): Char = {
+        val result = c.toChar
+        c = reader.read()
+        result
+    }
+}
+
+class ParseInt(it: Iterator[Char]) {
+    var st  = State.START
+    var result = 0L
+
+    def this(cs: CharSequence) = {
+        this(new StringIterator(cs))
+    }
+
+    def this(reader: Reader) = {
+        this(new ReaderIterator(reader))
+    }
+
+    // stub is this now
+    def parseLong(): Long = {
+        for (c <- it) {
+            val action: Function0[Unit] = transitionTable(st, c)
+            action()
+        }
+        result
+    }
+    
+    def toZERO(): Unit = {
+        st = State.ZERO
+    }
+
+    def toDEC(): Unit = {
+        result = 10 * result + (c - '0')
+        st = State.DEC
+    }
+
+    def toBIN(): Unit = {
+        result = 2 * result + (c - '0')
+        st = State.BIN
+    }
+
+    def toX(): Unit = {
+        st = State.X
+    }
+
+    def toHEX(): Unit = {
+        val d = if 'A' <= c && c <= 'F' then c - 'A' + 10
+                else if 'a' <= c && c <= 'f' then c - 'a' + 10
+                else c - '0'
+        
+        result = 16 * result + d
+        st = State.HEX
+    }
+}
