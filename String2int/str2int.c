20f8413923b1ce370f68e595534d495d69461d73
sasaki-shigeo <sasaki-shigeo@acm.org>
2025-06-18 21:31:04 +0900

    gotoで状態遷移

--- String2int/str2int.c
+++ String2int/str2int.c
@@ -4,105 +4,115 @@
 #include "str2int.h"
 
 long str2long(char *str) {
-    enum { ST_START, ST_ZERO, ST_DEC, ST_OCT, ST_X, ST_HEX } state = ST_START;
     long result = 0;
+    int c;
 
-    for (int c = *(str++); c != '\0'; c = *(str++)) {
-        switch (state) {
-            case ST_START:
-                if (c == '0') {
-                    state = ST_ZERO;
-                    continue;
-                }
-                else if (isdigit(c)) {
-                    state = ST_DEC;
-                    result = c - '0';
-                    continue;
-                }
-                else {
-                    return result;
-                }
-            case ST_ZERO:
-                if (c == 'x' || c == 'X') {
-                    state = ST_X;
-                    continue;
-                }
-                else if ('0' <= c && c <= '7') {
-                    state = ST_OCT;
-                    result = c - '0';
-                    continue;
-                }
-                else {
-                    return result;
-                }
-            case ST_DEC:
-                if (isdigit(c)) {
-                    result *= 10;
-                    result += c - '0';
-                    continue;
-                }
-                else {
-                    return result;
-                }
-            case ST_OCT:
-                if ('0' <= c && c <= '7') {
-                    result *= 8;
-                    result += c - '0';
-                    continue;
-                }
-                else {
-                    return result;
-                }
-            case ST_X:
-                if (isdigit(c)) {
-                    result = c - '0';
-                    continue;
-                }
-                else if ('A' <= c && c <= 'F') {
-                    result = c - 'A' + 10;
-                    continue;
-                }
-                else if ('a' <= c && c <= 'f') {
-                    result = c - 'a' + 10;
-                    continue;
-                }
-                else {
-                    return -1;
-                }
-            case ST_HEX:
-                if (isdigit(c)) {
-                    result *= 16;
-                    result += c - '0';
-                    continue;
-                }
-                else if ('A' <= c && c <= 'F') {
-                    result *= 16;
-                    result += c - 'A' + 10;
-                    continue;
-                }
-                else if ('a' <= c && c <= 'f') {
-                    result *= 16;
-                    result += c - 'a' + 10;
-                    continue;
-                }
-                else {
-                    return result;
-                }
-            default:
-                fprintf(stderr, "internal error\n");
-                exit(1);
-        }
+START:
+    c = *(str++);
+    if (c == '\0') {
+        return 0;
     }
+    else if (c == '0') {
+        goto ZERO;
+    }
+    else if (isdigit(c)) {
+        result = c - '0';
+        goto DEC;
+    }
+    else {
+        return result;
+    }
+
+ZERO:
+    c = *(str++);
+    if (c == 'x' || c == 'X') {
+        goto X;
+    }
+    else if ('0' <= c && c <= '7') {
+        result = c - '0';
+        goto OCT;
+    }
+    else {
+        return result;
+    }
+
+DEC:
+    c = *(str++);
+    if (isdigit(c)) {
+        result *= 10;
+        result += c - '0';
+        goto DEC;
+    }
+    else {
+        return result;
+    }
+
+OCT:
+    c = *(str++);
+    if ('0' <= c && c <= '7') {
+        result *= 8;
+        result += c - '0';
+        goto OCT;
+    }
+    else {
+        return result;
+    }
+
+X:
+    c = *(str++);
+    if (isdigit(c)) {
+        result = c - '0';
+        goto HEX;
+    }
+    else if ('A' <= c && c <= 'F') {
+        result = c - 'A' + 10;
+        goto HEX;
+    }
+    else if ('a' <= c && c <= 'f') {
+        result = c - 'a' + 10;
+        goto HEX;
+    }
+    else {
+        return -1;
+    }
+
+HEX:
+    c = *(str++);
+    if (isdigit(c)) {
+        result *= 16;
+        result += c - '0';
+        goto HEX;
+    }
+    else if ('A' <= c && c <= 'F') {
+        result *= 16;
+        result += c - 'A' + 10;
+        goto HEX;
+    }
+    else if ('a' <= c && c <= 'f') {
+        result *= 16;
+        result += c - 'a' + 10;
+        goto HEX;
+    }
+    else {
+        return result;
+    }
+
+ERROR:
+    fprintf(stderr, "internal error\n");
+    exit(1);
+
+ACCEPT:            /* use return result; instead goto ACCCEPT */
     return result;
 }
 
-
+// data for testing
 static char *literals[] = {
     "0", "123", "077", "0xff", "0XFF",
     NULL
 };
 
-int main() {
+// test function
+int main(void) {
     for (char **p = literals; *p != NULL; p++) {
         printf("%s = %d\n", *p, str2int(*p));
     }
